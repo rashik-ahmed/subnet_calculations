c85584d339c961c2d2d64b1dd9ec9cb599f92511
rashik-ahmed <ra3197@nyu.edu>
2024-11-02 13:41:01 -0400

    Update subnet.py

--- subnet.py
+++ subnet.py
@@ -1,65 +1,66 @@
-#enter your unique code
-uniquecode = 22360​
+# enter your unique code
+uniquecode = 22360
 
-#problem 1
+# Problem 1
 p1Network = '104.85.151.64'
 p1Broadcast = '104.85.151.127'
 p1First = '104.85.151.65'
-p1Last =  '104.85.151.126'
+p1Last = '104.85.151.126'
 
-#problem 2
+# Problem 2
 p2Network = '15.70.113.168'
 p2Broadcast = '15.70.113.175'
 p2First = '15.70.113.169'
 p2Last = '15.70.113.174'
 
-#problem 3
+# Problem 3
 p3Network = '182.55.74.192'
 p3Broadcast = '182.55.74.223'
 p3First = '182.55.74.193'
 p3Last = '182.55.74.222'
 
-#problem 4
+# Problem 4
 p4Network = '93.40.36.0'
 p4Broadcast = '93.40.36.255'
 p4First = '93.40.36.1'
 p4Last = '93.40.36.254'
 
-#problem 5
+# Problem 5
 p5Network = '4.25.254.32'
 p5Broadcast = '4.25.254.47'
 p5First = '4.25.254.33'
 p5Last = '4.25.254.46'
 
-#problem 6
+# Problem 6
 p6Network = '171.10.216.0'
 p6Broadcast = '171.10.216.127'
 p6First = '171.10.216.1'
-p6Last =  '171.10.216.126'
+p6Last = '171.10.216.126'
 
-#problem 7
+# Problem 7
 p7Network = '82.251.177.120'
 p7Broadcast = '82.251.177.127'
 p7First = '82.251.177.121'
 p7Last = '82.251.177.126'
 
-#problem 8
+# Problem 8
 p8Network = '249.237.139.128'
 p8Broadcast = '249.237.139.191'
 p8First = '249.237.139.129'
 p8Last = '249.237.139.190'
 
-#problem 9
+# Problem 9
 p9Network = '160.222.101.0'
 p9Broadcast = '160.222.101.255'
 p9First = '160.222.101.1'
 p9Last = '160.222.101.254'
 
-#problem 10
+# Problem 10
 p10Network = '71.207.63.224'
 p10Broadcast = '71.207.63.255'
 p10First = '71.207.63.225'
 p10Last = '71.207.63.254'
+
 
 
 """
